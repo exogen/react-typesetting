632444a0636f5b2489b37f20b3adf7f084d934b7
Brian Beck <exogen@gmail.com>
2018-10-06 01:43:28 -0700

    Fix esm build

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,12 @@
 module.exports = {
-  presets: ["next/babel"],
+  presets: [
+    [
+      "next/babel",
+      {
+        cjs: { "preset-env": { modules: "commonjs" } },
+        esm: { "preset-env": { modules: false } }
+      }[process.env.BABEL_ENV] || {}
+    ]
+  ],
   plugins: [["babel-plugin-styled-components", { ssr: true }]]
 };
